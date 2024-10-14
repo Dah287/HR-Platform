7106622ad533aeaf5180eba1704a810013e28bb9
dahman <dahmanb996@gmail.com>
2024-10-14 17:36:05 +0000

    add projett to github

---
+++ src/main/java/com/Dahman/HR_Platform/HrPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.Dahman.HR_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPlatformApplication.class, args);
+	}
+
+}
